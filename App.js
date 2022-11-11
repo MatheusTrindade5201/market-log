bd4b9bd52be44ce0002ac6ead5bba5b8839d64f3
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-11-10 21:41:09 -0300

    feat: setting up default colors

--- App.js
+++ App.js
@@ -4,8 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
@@ -13,8 +12,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFCCD1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: "#FFF"
+  }
 });
