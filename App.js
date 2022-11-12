d8d701037bbdc057ff35354d895789f05404b89c
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-11-11 21:16:48 -0300

    feat: Changing tags to adpat the view size in any device

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar />
+      
+    </SafeAreaView>
   );
 }
 
@@ -14,8 +14,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFCCD1',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   text: {
     color: "#FFF"
